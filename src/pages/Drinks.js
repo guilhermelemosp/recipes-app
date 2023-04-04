7c2a7766004936d1fc4470499bb632ae9da2a28a
Katia Mayumi Yamamoto <mayumee@gmail.com>
2023-04-04 16:23:13 -0300

    Reqs 16, 17 e 18

--- src/pages/Drinks.js
+++ src/pages/Drinks.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function Drinks() {
   return (
-    <div><Header isRender namePage="Drinks" /></div>
+    <>
+      <div><Header isRender namePage="Drinks" /></div>
+      <div><Footer /></div>
+    </>
   );
 }
--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 export default function Meals() {
   return (
-    <div><Header isRender namePage="Meals" /></div>
+    <>
+      <div><Header isRender namePage="Meals" /></div>
+      <div><Footer /></div>
+    </>
   );
 }

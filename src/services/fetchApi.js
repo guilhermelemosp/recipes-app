7985c1bd3b2d832e752b39e75c23397906402f6d
Guilherme Lemos <guilhermelemospimentel@gmail.com>
2023-04-05 18:05:11 -0300

    requisito 13 feito

--- src/services/fetchApi.js
+++ src/services/fetchApi.js
@@ -1,35 +1,90 @@
-export const getIngredient = async (ingredient) => {
-  const request = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`);
-  const requestJson = await request.json();
-  return requestJson;
+const termNotFound = 'Term Not Found';
+const noRecipeFound = 'No Recipe Found, sorry :(';
+
+export const MealsAPI = async (radioInput, termName) => {
+  if (radioInput === 'Ingredient') {
+    if (!termName) {
+      global.alert(termNotFound);
+      return 'Error';
+    }
+    const getIngredient = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${termName}`);
+    const requestJson = await getIngredient.json();
+    if (requestJson.meals === null) {
+      global.alert(noRecipeFound);
+      return 'Error';
+    }
+    return requestJson;
+  }
+
+  if (radioInput === 'Name') {
+    if (!termName) {
+      global.alert(termNotFound);
+      return 'Error';
+    }
+    const request = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${termName}`);
+    const requestJson = await request.json();
+    if (requestJson.meals === null) {
+      global.alert(noRecipeFound);
+      return 'Error';
+    }
+    return requestJson;
+  }
+
+  if (radioInput === 'FirstLetter') {
+    if (!termName) {
+      global.alert(termNotFound);
+      return 'Error';
+    }
+    if (termName.length > 1) {
+      global.alert('Your search must have only 1 (one) character');
+      return 'Error';
+    }
+    const request = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${termName}`);
+    const requestJson = await request.json();
+    return requestJson;
+  }
 };
 
-export const getName = async (name) => {
-  const request = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`);
-  const requestJson = await request.json();
-  return requestJson;
-};
+export const DrinksAPI = async (radioInput, termName) => {
+  if (radioInput === 'Ingredient') {
+    if (!termName) {
+      global.alert(termNotFound);
+      return 'Error';
+    }
+    const getDrinkIng = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${termName}`);
+    const requestJson = await getDrinkIng.json();
+    if (requestJson.drinks === null) {
+      global.alert(noRecipeFound);
+      return 'Error';
+    }
+    return requestJson;
+  }
 
-export const getFirstL = async (fistL) => {
-  const request = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${fistL}`);
-  const requestJson = await request.json();
-  return requestJson;
-};
+  if (radioInput === 'Name') {
+    if (!termName) {
+      global.alert(termNotFound);
+      return 'Error';
+    }
+    const getDrinkName = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${termName}`);
+    const requestJson = await getDrinkName.json();
+    if (requestJson.drinks === null) {
+      global.alert(noRecipeFound);
+      return 'Error';
+    }
+    return requestJson;
+  }
 
-export const getDrinkIng = async (ingredient) => {
-  const request = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`);
-  const requestJson = await request.json();
-  return requestJson;
-};
-
-export const getDrinkName = async (name) => {
-  const request = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`);
-  const requestJson = await request.json();
-  return requestJson;
-};
-
-export const getDrinkFirstL = async (fistL) => {
-  const request = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${fistL}`);
-  const requestJson = await request.json();
-  return requestJson;
+  if (radioInput === 'FirstLetter') {
+    if (!termName) {
+      global.alert(termNotFound);
+      return 'Error';
+    }
+    if (termName.length > 1) {
+      global.alert('Your search must have only 1 (one) character');
+      return 'Error';
+    }
+    const getDrinkFirstL = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${termName}`);
+    const requestJson = await getDrinkFirstL.json();
+    return requestJson;
+  }
 };
